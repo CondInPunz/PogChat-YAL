14d22d6d8fb64bc57615e36681c08f1b44c3ab36
CondInPunz <VectorAs@yandex.ru>
2021-11-22 17:30:15 +0600

    Remade all connection system.

--- Server.py
+++ Server.py
@@ -1,28 +1,29 @@
 import socket
 import db_operations
 
-host = socket.gethostbyname(socket.gethostname())
-port = 9090
-db_name = 'userdata.sqlite'
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('', port))
 LOG_IN = 0
 SEND = 1
 LOG_OUT = 2
 SELECT_ALL_MESS = 3
 SELECT_LAST_MESS = 4
+REG = 5
+db_name = 'userdata.sqlite'
 
-server_socket.listen(1)
+host = socket.gethostbyname(socket.gethostname())
+print(host)
+port = 9091
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind((host, port))
+
 print('server started!')
-conn, addr = server_socket.accept()
 
-print(f'{addr} joined the chat!')
 shutdown_server = False
-is_logged_in = False
 
 while not shutdown_server:
     try:
-        data = conn.recv(1024)
+        data, client_address = server_socket.recvfrom(2048)
+        print(data)
         data = data.decode('utf-8')
         print(data)
         query = int(data.split()[0])
@@ -32,47 +33,42 @@ while not shutdown_server:
 
         if query == SEND:
             print('all is ok')
-            if is_logged_in:
-                message = extra_data
-                print(message)
-                db_operations.send_message(db_name, client_login, message)
-                conn.send(client_login.encode('utf-8').upper())
-                print('cool')
-            else:
-                conn.send('not logged in'.encode('utf-8'))
+            message = extra_data
+            print(message)
+            db_operations.send_message(db_name, client_login, message)
+            server_socket.sendto(client_login.encode('utf-8').upper(), client_address)
+            print('cool')
 
         if query == LOG_IN:
-            # print('all is ok')
+            print('all is ok')
             password = extra_data
             if db_operations.find_user(db_name, client_login):
-                # print('all is ok')
                 if db_operations.check_password(db_name, client_login, password):
-                    conn.send('u joined chat'.encode('utf-8'))
+                    server_socket.sendto('u joined chat'.encode('utf-8'), client_address)
+                    print('user joined chat')
                     is_logged_in = True
                     db_operations.send_message(db_name, '[SERVER]', f'{client_login} joined chat!')
                 else:
-                    conn.send('u r not the father'.encode('utf-8'))
-                    # print('error sent')
+                    server_socket.sendto('u r not the father'.encode('utf-8'), client_address)
+                    print('error sent')
                 db_operations.set_status(db_name, client_login, True)
             else:
-                conn.send('new_challenger_appears'.encode('utf-8'))
-                print('new_user appeared')
+                server_socket.sendto('who r u'.encode('utf-8'), client_address)
 
         if query == LOG_OUT:
             db_operations.set_status(db_name, client_login, False)
             is_logged_in = False
             db_operations.send_message(db_name, '[SERVER]', f'[{client_login} joined chat!]')
-            conn.send('u left chat'.encode('utf-8'))
 
         if query == SELECT_ALL_MESS:
             data = db_operations.select_all_messages(db_name)
-            conn.send(data.encode('utf-8'))
             print('all messages sent to client successfully.')
 
-        if query == SELECT_LAST_MESS:
-            data = db_operations.select_last_message(db_name)
-            conn.send(data.encode('utf-8'))
-            print('the last message sent to client successfully.')
+        if query == REG:
+            password = extra_data
+            print(client_login, password)
+            db_operations.add_user(db_name, client_login, password)
+            print('new user created.')
     except:
         shutdown_server = True
 
